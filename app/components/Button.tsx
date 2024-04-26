4ffe61c7b4ddc51d9f3cea5af52c6461705f560f
YManjunath <manjuyatnatti@gmail.com>
2024-04-26 11:03:20 +0530

    build fail fix

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,4 +1,6 @@
-const Button = ({ children }) => {
+import { ReactNode } from "react";
+
+const Button = ({ children }: { children: ReactNode }) => {
     return (
       <button className="lg:py-5 py-3  font-mono outline-none px-5 lg:px-7 bg-yellow-500 hover:bg-yellow-400 text-white rounded-full shadow-xl">
         {children}
