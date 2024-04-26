d43e2b2c6363c28d2fda9cbf456a52ce263cbaf0
YManjunath <manjuyatnatti@gmail.com>
2024-04-26 11:07:26 +0530

    build fail fix

--- app/components/MainLayout.tsx
+++ app/components/MainLayout.tsx
@@ -1,4 +1,6 @@
-const MainLayout = ({ children }) => {
+import { ReactNode } from "react";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
     return <main className="lg:max-w-7xl lg:mx-auto ">{children}</main>;
   };
   
